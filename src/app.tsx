70180b8e126003b6cff9331686a281784a38b225
Vegard Angell <vegard.angell@energima.no>
2023-11-10 09:16:13 +0100

    bugfix: - tailwind works!

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,6 @@
 export default function App () {
   return (
-    <div className={"p-4 bg-blue-300"}>
+    <div className={"p-4 bg-blue-300 w-1/2 min-w-[200px] mx-auto"}>
       <h1>Hello world!</h1>
     </div>
   )
