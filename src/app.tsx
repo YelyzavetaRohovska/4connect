835c14c9fc031316c21952713053d6c772fa02d6
Vegard Angell <vegard.angell@scienta.no>
2024-05-29 13:38:40 +0200

    cleanup, update packages

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,8 @@
-import { V1234 } from './release-files'
 
 export default function App () {
   return (
     <div className={"p-4 bg-blue-300 w-1/2 min-w-[200px] mx-auto"}>
       <h1>Hello world!</h1>
-      <V1234 />
     </div>
   )
 }
--- src/release-files/index.ts
+++
@@ -1,3 +0,0 @@
-import V1234 from './v1234.mdx'
-
-export { V1234 }
