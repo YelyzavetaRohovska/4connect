1436d45aacaae21fd63096fb88f1ea96b99b6120
Vegard Angell <vegard.angell@scienta.no>
2024-05-29 15:06:36 +0200

    Make it possible to both read and write Rooms

--- src/api.ts
+++ src/api.ts
@@ -1,11 +1,43 @@
-import {rooms} from "./data/rooms";
-import {tree} from "./data/tree";
-import {Room, RoomTreeNode} from "./data/types";
+import { rooms } from "./data/rooms"
+import { tree } from "./data/tree"
+import { Rooms, Room, RoomTreeNode } from "./data/types"
 
-export const getRooms = ():Promise<RoomTreeNode[]>=> {
-    return Promise.resolve(tree).delay(2000);
-}
+let _instance = null
+const API_DELAY_MS = 500
 
-export const getRoom = (id: number):Promise<Room>=> {
-    return Promise.resolve(rooms[id]).delay(2000);
+class Api {
+  rooms: Rooms = { ...rooms }
+  tree: RoomTreeNode[] = [...tree]
+
+  constructor() {
+    if (_instance === null) {
+      _instance = this
+    }
+    return _instance
+  }
+
+  /**
+   * List of RoomTreeNode elements that can be used to build a tree
+   */
+  getRooms(): Promise<RoomTreeNode[]> {
+    return Promise.resolve(this.tree).delay(API_DELAY_MS)
+  }
+
+  /**
+   * fetch a single Room
+   * @param id
+   */
+  getRoom(id: number): Promise<Room> {
+    return Promise.resolve(this.rooms[id]).delay(API_DELAY_MS)
+  }
+
+  /**
+   * Update a single Room
+   * @param id
+   * @param room
+   */
+  setRoom(id: number, room: Room): Promise<Room> {
+    this.rooms[id] = room
+    return this.getRoom(id)
+  }
 }
